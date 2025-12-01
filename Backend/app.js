100f3b82774a6bbf8d3ea8edfd67f717353e9f35
Vivek <vivekghule777@gmail.com>
2025-12-01 22:01:19 +0530

    refactor: streamline dotenv configuration and enhance middleware setup

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,33 @@
-
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 const express = require('express')
 const cors = require('cors');
 const app = express()
+const cookieParser = require('cookie-parser')
+const connectToDB = require('./db/db')
+const userRoutes = require('./Routes/user.routes')
+
+
+
+connectToDB();
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
+
+
+
+
+
 app.get('/', (req,res)=>{
 
     res.send("Hello World")
 })
 
+app.use('/users',userRoutes)
 
 
+ 
 
 
 
